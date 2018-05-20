d62c272cf14cab4b42014c024fe3a4b27ab67dc6
Antonio <retonho@gmail.com>
2018-05-20 21:58:36 +0200

    Primer commit P6

---
+++ routes/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router = express.Router();
+
+const quizController = require('../controllers/quiz');
+
+/* GET home page. */
+router.get('/', (req, res, next) => {
+  res.render('index');
+});
+
+// Author page.
+router.get('/author', (req, res, next) => {
+    res.render('author');
+});
+
+
+// Autoload for routes using :quizId
+router.param('quizId', quizController.load);
+
+
+// Routes for the resource /quizzes
+router.get('/quizzes',                     quizController.index);
+router.get('/quizzes/:quizId(\\d+)',       quizController.show);
+router.get('/quizzes/new',                 quizController.new);
+router.post('/quizzes',                    quizController.create);
+router.get('/quizzes/:quizId(\\d+)/edit',  quizController.edit);
+router.put('/quizzes/:quizId(\\d+)',       quizController.update);
+router.delete('/quizzes/:quizId(\\d+)',    quizController.destroy);
+
+router.get('/quizzes/:quizId(\\d+)/play',  quizController.play);
+router.get('/quizzes/:quizId(\\d+)/check', quizController.check);
+
+
+module.exports = router;
